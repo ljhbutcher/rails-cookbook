c02f3ce466d7b76d2742cb0cda6232f0d02c10a2
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-13 09:55:31 +0000

    seed done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,5 @@ class Recipe < ApplicationRecord
   has_many :categories, through: :bookmarks
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
