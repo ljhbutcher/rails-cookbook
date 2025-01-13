5004ff964e3074f876f2c263047a531f7dbce0ec
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-13 09:43:43 +0000

    associations added

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+  validates :comment, length: { minimum: 6 }
+  validates :recipe_id, uniqueness: { scope: :category_id }
 end
